bbde7b87c715828e5518e2b6442fe1985a211d16
Maasa Kono <konomaasa@gmail.com>
2019-05-29 17:59:28 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,32 @@
+#OLD HASH
+# {
+#   :oo => {
+#     :ruby => {
+#       :type => "interpreted"
+#     },
+#     :javascript => {
+#       :type => "interpreted"
+#     },
+
+#NEW HASH
+# {
+#   :ruby => {
+#     :type => "interpreted",
+#     :style => [:oo]
+#   },
+#   :javascript => {
+#     :type => "interpreted",
+#     :style => [:oo, :functional]
+#   },
+
+#Use []= to create key/value pairs
+#Use << to add items to array
+
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, name|
+    binding.pry
+  end
 end
