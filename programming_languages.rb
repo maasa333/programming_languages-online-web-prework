8ed75738f3a40c7c3396e9dd047834dd42318d0c
Maasa Kono <konomaasa@gmail.com>
2019-06-01 14:20:14 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,5 +1,5 @@
 #OLD HASH
-# {
+# languages = {
 #   :oo => {
 #     :ruby => {
 #       :type => "interpreted"
@@ -7,33 +7,32 @@
 #     :javascript => {
 #       :type => "interpreted"
 #     },
+#     :python => {
+#       :type => "interpreted"
+#     },
+#     :java => {
+#       :type => "compiled"
+#     }
+#   },
 
 #NEW HASH
-# {
-#   :ruby => {
-#     :type => "interpreted",
-#     :style => [:oo]
-#   },
-#   :javascript => {
-#     :type => "interpreted",
-#     :style => [:oo, :functional]
-#   },
-
   # ruby = {:type => "interpreted", :style => [:oo]}
 
 #Use []= to create key/value pairs
 #Use << to add items to array
 
 # 1. Method takes argument of a hash.
-# 2. Iterates over each nested hash to take the each key-value pairs of language and type, and insert into new hash.
+# 2. Iterates over each nested hash to take each key-value pairs of language and type, and insert into new hash.
 # 3. Iterates over the hash to pull the style and push it into an array of keys for each lanuguage in the new hash.
 
 require 'pry'
 
 def reformat_languages(languages)
   new_hash = {}
-  languages.map do |style, name|
+  languages.each do |style, language|
+    language.each do |t, v|
+
     binding.pry
-    new_hash[:language] = name
+    end
   end
 end
