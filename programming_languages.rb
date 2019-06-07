31e5f0122e2a70741581e057ba003bdbcec425ed
Maasa Kono <konomaasa@gmail.com>
2019-06-07 00:07:23 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -25,14 +25,19 @@
 # 2. Iterates over each nested hash to take each key-value pairs of language and type, and insert into new hash.
 # 3. Iterates over the hash to pull the style and push it into an array of keys for each lanuguage in the new hash.
 
+#I'm using the .map method because for some reason I couldn't use .each.
+
 require 'pry'
 
 def reformat_languages(languages)
   new_hash = {}
-  languages.each do |style, language|
-    language.each do |t, v|
-
-    binding.pry
+  languages.map do |style, lang|
+    lang.map do |typ, vlu|
+      binding.pry
+      new_hash[typ] = {}
+      new_hash[typ][:style] ||= []
+      new_hash[typ][:style] << style
     end
   end
+  new_hash
 end
